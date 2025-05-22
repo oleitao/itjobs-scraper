9a3ac4b36f8a29e78da2ceab4ba2d1f03566af84
olivleitao <olivleitao@gmail.com>
2025-05-22 12:10:18 +0100

    fix

--- load.py
+++ load.py
@@ -1,41 +1,62 @@
 import mysql.connector
 import simplejson as json
 import sys
+import re
 
 unique_companies = sys.argv[1]
-print("test")
-print(unique_companies)
 
+config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+    "database": "dbWebRH-Dev"
+}
 
+try:
+    # Conectar ao MySQL
+    conn = mysql.connector.connect(**config)
 
-# try:
-#     # Conectar ao MySQL
-#     conn = mysql.connector.connect(**config)
-#     cursor = conn.cursor()
+    # Check if connection is successful
+    if conn.is_connected():
+        print("Connected to MySQL successfully!")
 
-#     # Crie a tabela se ainda não existir
-#     cursor.execute("""
-#         CREATE TABLE IF NOT EXISTS companies (
-#             id INT AUTO_INCREMENT PRIMARY KEY,
-#             name VARCHAR(255) UNIQUE
-#         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-#     """)
+    cursor = conn.cursor()
 
-#     # Inserir empresas
-#     for company in unique_companies:
-#         try:
-#             cursor.execute("INSERT IGNORE INTO companies (name) VALUES (%s)", (company,))
-#         except Exception as e:
-#             print(f"Erro ao inserir '{company}': {e}")
+    # Crie a tabela se ainda não existir
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS companies (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) UNIQUE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
+   
+    companies_list = re.findall(r"'(.*?)'", unique_companies)
 
-#     # Salvar alterações
-#     conn.commit()
-#     print("Empresas inseridas com sucesso!")
+    # Convert to JSON
+    companies_json = json.dumps(companies_list, ensure_ascii=False, indent=2)
+    companies_list = json.loads(companies_json)
 
-# except mysql.connector.Error as err:
-#     print(f"Erro de conexão ou execução: {err}")
-# finally:
-#     if 'cursor' in locals():
-#         cursor.close()
-#     if 'conn' in locals():
-#         conn.close()
+    # print(companies_json)
+
+    # for company in companies_list:
+    #     print(company)
+
+    # Inserir empresas
+    for company in companies_list:
+        try:
+            cursor.execute("INSERT IGNORE INTO companies (name) VALUES (%s)", (company,))
+            print(company)
+        except Exception as e:
+            print(f"Erro ao inserir '{company}': {e}")
+
+    # Salvar alterações
+    conn.commit()
+    print("Empresas inseridas com sucesso!")
+
+except mysql.connector.Error as err:
+    print(f"Erro de conexão ou execução: {err}")
+finally:
+    if 'cursor' in locals():
+        cursor.close()
+    if 'conn' in locals():
+        conn.close()
